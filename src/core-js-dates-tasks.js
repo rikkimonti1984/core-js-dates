5287251da6cb4c83151e485101dcacaa1da44811
Rikki Monti <rikkimonti@gmail.com>
2024-03-25 10:07:37 +0500

    feat: implement getCountDaysOnPeriod function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -125,8 +125,13 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startDate = new Date(dateStart);
+  const endDate = new Date(dateEnd);
+
+  const diff = endDate - startDate;
+
+  return Math.floor(diff / (1000 * 60 * 60 * 24)) + 1;
 }
 
 /**

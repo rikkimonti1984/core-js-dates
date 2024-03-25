a735a053c7c94e82a69ca6570b9a9536be951667
Rikki Monti <rikkimonti@gmail.com>
2024-03-25 08:55:48 +0500

    feat: implement getCountDaysInMonth function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -106,8 +106,12 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const nextMonth = new Date(year, month, 1);
+
+  nextMonth.setDate(nextMonth.getDate() - 1);
+
+  return nextMonth.getDate();
 }
 
 /**
